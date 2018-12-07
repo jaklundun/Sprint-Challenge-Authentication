7faadeb19e51f49831c8ed570784f9d74af9836c
jaklundun <govi.escudero@gmail.com>
2018-12-07 10:26:40 -0800

    Implemented login helper function.

--- config/routes.js
+++ config/routes.js
@@ -26,6 +26,28 @@ function generateToken(user) {
    return jwt.sign(payload, secret, options);
 }
 
+  function login(req, res) {
+    // implement user login
+    const creds = req.body;
+    if (!creds.username|| !creds.password) {
+      res.status(400).json({ error: "You'll need to give up both your userId and Pass, homie." });
+      return
+  } 
+    db('users')
+      .where({ username: creds.username })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(creds.password, user.password)) {
+          const token = generateToken(user);
+          res.status(200).json({ message: 'Benvenuto!', token });
+        } else {
+          res.status(401).json({ message: 'Not unauthorized access - Scram!' });
+        }
+      })
+      .catch(err => res.json(err));
+  };
+
+
 function register(req, res) {
   // implement user registration
  const creds = req.body;
